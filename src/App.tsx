1a485e945419e16a6733b1f184470fb7e054f711
Maha Taha <abdelrahman.osama@kashat.com.eg>
2022-01-25 10:59:20 +0200

    initiate project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
+        <h1 className="text-9xl font-black text-white text-center">
+          <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+            heeeelooo
+          </span>
+        </h1>
+      </div>
     </div>
   );
 }
